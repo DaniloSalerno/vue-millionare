105526d98d9d0292a9e28a70a7e75b4fb9a1b867
DaniloSalerno <salerno.danilo@hotmail.it>
2024-02-15 18:02:42 +0100

    initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'AppHeader',
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+header {
+  height: 35vh;
+  background-color: $primary_dark;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'AppMain',
+
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+main {
+    height: 65vh;
+    background-color: $primary_light;
+}
+</style>
